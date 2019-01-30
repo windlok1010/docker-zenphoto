45cdf2498a0f04f9ce08cae644a4699e57890199
benderstwin <bryan@bwirth.com>
2019-01-30 08:13:59 -0700

    variables

--- run.sh
+++ run.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 BASEDIR=/var/www/html
+MYSQL_USER=zenphoto
+MYSQL_DB=zenphoto
 # extract and initialize zenphoto if not present
 if [ ! -d $BASEDIR/zp-core ]; then
 	rm $BASEDIR/index.html
 	# extract zenphoto files
 	/bin/tar xvf /zenphoto.tar.gz -C $BASEDIR --strip-components=1
 	# create config
+	/bin/cp $BASEDIR/zp-core/zenphoto_cfg.txt $BASEDIR/zp-data/zenphoto.cfg.php
+	sed -i "/mysql_user/c\$conf['mysql_user'] = '$MYSQL_USER';" $BASEDIR/zp-data/zenphoto.cfg.php
+	sed -i "/mysql_database/c\$conf['mysql_database'] = '$MYSQL_DB';" $BASEDIR/zp-data/zenphoto.cfg.php
+	sed -i "/mysql_database/c\$conf['mysql_host'] = '$MYSQL_HOST';" $BASEDIR/zp-data/zenphoto.cfg.php
+	sed -i "/mysql_database/c\$conf['mysql_pass'] = '$MYSQL_PASS';" $BASEDIR/zp-data/zenphoto.cfg.php
 	mkdir $BASEDIR/cache
 	mkdir $BASEDIR/cache_html
 	chown www-data $BASEDIR/albums
